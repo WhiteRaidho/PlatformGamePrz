e1be1375fb325e6838e6227791e1cee3fbbbd468
Kamil Wlazlo <kwlazlo10@gmail.com>
2020-06-18 21:44:04 +0200

    Prefabs and Editors Scripts

---
+++ PlatformGame/Assets/Scripts/Editor/PipeCustomEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Pipe))]
+public class PipeCutomEditor : Editor
+{
+    Pipe pipe;
+
+    private void OnEnable()
+    {
+        pipe = (Pipe)target;
+    }
+
+    private void OnSceneGUI()
+    {
+        for(int i = 0; i < pipe.nodes.Count - 1; i++)
+        {
+            Handles.color = Color.red;
+            //Handles.SphereCap(0, pipe.nodes[i], Quaternion.identity, 0.2f);
+            Handles.color = Color.cyan;
+            Handles.DrawLine(pipe.nodes[i], pipe.nodes[i + 1]);
+        }
+    }
+}
